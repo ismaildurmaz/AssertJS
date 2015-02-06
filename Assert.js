3b1ae9e1ccab383ad433e153a8081e3f0ab265ca
ismail durmaz <ismaildurmaz85@hotmail.com>
2015-02-06 13:06:58 +0200

    updated README.md bug fix

--- Assert.js
+++ Assert.js
@@ -1,7 +1,27 @@
-/**
- * written by ismail durmaz
+/*
+ * Copyright 2015 ismail durmaz https://github.com/ismaildurmaz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 Assert = {
+	/**
+	 * @private
+	 * @param value
+	 * @param typeName
+	 * @returns {boolean}
+	 * @private
+	 */
 	_isType: function ( value, typeName ) {
 		return Object.prototype.toString.apply( value ) === '[object ' + typeName + ']';
 	},
@@ -16,6 +36,7 @@ Assert = {
 	},
 
 	/**
+	 * @private
 	 * if value is false, Assertion throws @param message
 	 * @param value
 	 * @param message
@@ -67,15 +88,15 @@ Assert = {
 	},
 
 	isGreater: function ( value, condition, message ) {
-		return Assert.handleAssert( value > condition, message, '[' + value + ' < x]', value );
+		return Assert.handleAssert( value > condition, message, '[' + condition + ' < x]', value );
 	},
 
 	isGreaterEqual: function ( value, condition, message ) {
-		return Assert.handleAssert( value >= condition, message, '[' + value + ' <= x]', value );
+		return Assert.handleAssert( value >= condition, message, '[' + condition + ' <= x]', value );
 	},
 
 	isLower: function ( value, condition, message ) {
-		return Assert.handleAssert( value < condition, message, '[' + value + ' > x]', value );
+		return Assert.handleAssert( value < condition, message, '[' + condition + ' > x]', value );
 	},
 
 	isLowerEqual: function ( value, condition, message ) {
@@ -86,55 +107,97 @@ Assert = {
 		return Assert.handleAssert( value === false, message, false, value );
 	},
 
+	/**
+	 * @private
+	 * @param value
+	 * @param typeName
+	 * @returns {boolean}
+	 * @private
+	 */
 	_isNumber: function ( value ) {
 		return Assert._isType( value, 'Number' );
 	},
 
 	isNumber: function ( value, message ) {
-		return Assert.handleAssert( Assert._isNumber( value ), message, 'number', value );
+		return Assert.handleAssert( Assert._isNumber( value ), message, '[NUMBER]', value );
 	},
 
+	/**
+	 * @private
+	 * @param value
+	 * @param typeName
+	 * @returns {boolean}
+	 * @private
+	 */
 	_isString: function ( value ) {
 		return Assert._isType( value, 'String' );
 	},
 
 	isString: function ( value, message ) {
-		return Assert.handleAssert( Assert._isString( value ), message, 'string', value );
+		return Assert.handleAssert( Assert._isString( value ), message, '[STRING]', value );
 	},
 
+	/**
+	 * @private
+	 * @param value
+	 * @param typeName
+	 * @returns {boolean}
+	 * @private
+	 */
 	_isArray: function ( value, message ) {
 		return Assert._isType( value, 'Array' );
 	},
 
 	isArray: function ( value, message ) {
-		return Assert.handleAssert( Assert._isArray( value ), message, 'array', value );
+		return Assert.handleAssert( Assert._isArray( value ), message, '[ARRAY]', value );
 	},
 
+	/**
+	 * @private
+	 * @param value
+	 * @param typeName
+	 * @returns {boolean}
+	 * @private
+	 */
 	_isDate: function ( value, message ) {
 		return Assert._isType( value, 'Date' );
 	},
 
 	isDate: function ( value, message ) {
-		return Assert.handleAssert( Assert._isDate( value ), message, 'date', value );
+		return Assert.handleAssert( Assert._isDate( value ), message, '[DATE]', value );
 	},
 
+	/**
+	 * @private
+	 * @param value
+	 * @param typeName
+	 * @returns {boolean}
+	 * @private
+	 */
 	_isDefined: function ( value, message ) {
 		return typeof value !== "undefined";
 	},
 
 	isDefined: function ( value, message ) {
-		return Assert.handleAssert( Assert._isDefined( value ), message, 'defined', value );
+		return Assert.handleAssert( Assert._isDefined( value ), message, '[DEFINED]', value );
 	},
 
+	/**
+	 * @private
+	 * @param value
+	 * @param typeName
+	 * @returns {boolean}
+	 * @private
+	 */
 	_isUndefined: function ( value, message ) {
 		return typeof value === "undefined";
 	},
 
 	isUndefined: function ( value, message ) {
-		return Assert.handleAssert( Assert._isUndefined( value ), message, 'undefined', value );
+		return Assert.handleAssert( Assert._isUndefined( value ), message, '[UNDEFINED]', value );
 	},
 
-	isEmail: function ( value, message ) {
+	validateEmail: function ( value, message ) {
 		return Assert.validateRegex( value, /\S+@\S+\.\S+/, message );
 	},
 
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # AssertJS
 Assert Library For Javascript
+
+No library required.
+
+# LOGICAL
+```javascript
+Assert.isTrue(false, 'Value must be true');	// FAILS
+Assert.isTrue(true, 'Value must be true'); // OK
+
+Assert.isFalse(true, 'Value must be false');	// FAILS
+Assert.isFalse(false, 'Value must be true');// OK
+
+Assert.isEqual(4, 5, 'Values must be the same');	// FAILS
+Assert.isEqual(4, "4", 'Values must be the same');	// OK
+
+Assert.isNotEqual("34.2", 34.2, 'Values must be the same');	// FAILS
+Assert.isNotEqual(new Date(), null, 'Values must be the same');	// OK
+
+Assert.isEmpty([5], 'Array should be empty');	// FAILS
+Assert.isEmpty([], 'Array should be empty');	// OK
+Assert.isEmpty("", 'String should be empty');	// OK
+
+Assert.isNotEmpty([], 'Array should be not empty');	// FAILS
+Assert.isNotEmpty([5], 'Array should be not empty');	// OK
+Assert.isNotEmpty("4", 'String should be not empty');	// OK
+
+Assert.isGreater(5, 8, 'Current value should be greater than expected');	// FAILS
+Assert.isGreaterEqual(8, 8, 'Current value should be greater than expected');	// OK
+Assert.isLower(5, 8, 'Current value should be greater than expected');	// OK
+Assert.isLowerEqual(8, 8, 'Current value should be greater than expected');	// OK
+```
+
+# TYPES
+```javascript
+Assert.isDefined(undefined, 'Value must be defined');// FAILS
+Assert.isDefined(5, 'Value must be defined');// OK
+
+Assert.isUndefined(5, 'Value must be undefined');// FAILS
+Assert.isUndefined(undefined, 'Value must be undefined');// OK
+
+Assert.isNull(undefined, 'Value must be null');// FAILS
+Assert.isNull(null, 'Value must be null');// OK
+
+Assert.isNotNull(null, 'Value must be null');// FAILS
+Assert.isNotNull(!null, 'Value must be null');// OK
+
+Assert.isArray(5, 'Value must be an array');// FAILS
+Assert.isString([5], 'Value must be a string');// OK
+
+Assert.isNumber("5", 'Value must be a number');// FAILS
+Assert.isNumber(5, 'Value must be a number');// OK
+
+Assert.isDate(5, 'Value must be a date');// FAILS
+Assert.isDate(new Date(5), 'Value must be a date');// OK
+
+Assert.isString(5, 'Value must be a string');// FAILS
+Assert.isString("5", 'Value must be a string');// OK
+```
+
+# REGEX
+```javascript
+Assert.validateEmail('test', 'Value must be an email');// FAILS
+Assert.validateRegex('123.4', /[0-9]*\.[0-9]*/, 'Value must be valid');// OK
+```
